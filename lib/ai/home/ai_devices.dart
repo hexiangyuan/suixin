818bfd6bcc1cab27feccbf4eb3fb458b923f005a
xiangyuan.he <yyhe@btc088.com>
2023-11-18 20:42:22 +0800

    feat:提交

--- lib/ai/home/ai_devices.dart
+++ lib/ai/home/ai_devices.dart
@@ -1,8 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:suixin_app/ai/common/context_extension.dart';
+import 'package:suixin_app/ai/home/main.dart';
 import 'package:suixin_app/ai/theme/typography.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+import '../xt_button.dart';
 
 class AiDevicesManagerPage extends StatefulWidget {
   const AiDevicesManagerPage({super.key});
@@ -16,149 +20,153 @@ class _AiDevicesManagerPageState extends State<AiDevicesManagerPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child: CachedNetworkImage(
-                    imageUrl: "https://via.placeholder.com/150",
-                    width: 100,
-                    height: 100,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "设备名称",
-                      style: context.h2(),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      "电量 60%",
-                      style: context.h5(),
-                    )
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(height: 24),
-            InkWell(
-              onTap: () {},
-              child: Row(
+    return Scaffold(
+      appBar: AppBar(title: const Text("查看设备")),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "储存空间",
-                    style: context.h3(),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: CachedNetworkImage(
+                      imageUrl: "https://via.placeholder.com/150",
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  Expanded(child: Container()),
-                  Text(
-                    "12M/64G",
-                    style: context.h3(),
+                  const SizedBox(
+                    width: 12,
                   ),
-                  const SizedBox(width: 6),
-                  const Icon(Icons.arrow_right)
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "设备名称",
+                        style: context.h2(),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "电量 60%",
+                        style: context.h5(),
+                      )
+                    ],
+                  )
                 ],
               ),
-            ),
-            const SizedBox(height: 18),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Text(
-                  "设备音量",
-                  style: context.h3(),
+              const SizedBox(height: 24),
+              InkWell(
+                onTap: () {},
+                child: Row(
+                  children: [
+                    Text(
+                      "储存空间",
+                      style: context.h3(),
+                    ),
+                    Expanded(child: Container()),
+                    Text(
+                      "12M/64G",
+                      style: context.h3(),
+                    ),
+                    const SizedBox(width: 6),
+                    const Icon(Icons.arrow_right)
+                  ],
                 ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Expanded(
-                  child: Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                    alignment: Alignment.center,
-                    decoration: ShapeDecoration(
-                        color: context.xtColors().backgroundTertiary,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5))),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            if (volume > 1) {
-                              setState(() {
-                                volume--;
-                              });
-                            }
-                          },
-                          child: const Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 12),
-                              child: Text(
-                                "-",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.bold),
-                              )),
-                        ),
-                        Expanded(
-                          child: Center(
-                            child: Text(
-                              "$volume",
-                              style: context.h3(),
-                            ),
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            if (volume < 10) {
-                              setState(() {
-                                volume++;
-                              });
-                            }
-                          },
-                          child: const Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 12),
-                              child: Text("+",
+              ),
+              const SizedBox(height: 18),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    "设备音量",
+                    style: context.h3(),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 6),
+                      alignment: Alignment.center,
+                      decoration: ShapeDecoration(
+                          color: context.xtColors().backgroundTertiary,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              if (volume > 1) {
+                                setState(() {
+                                  volume--;
+                                });
+                              }
+                            },
+                            child: const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 12),
+                                child: Text(
+                                  "-",
                                   style: TextStyle(
                                       fontSize: 18,
-                                      fontWeight: FontWeight.bold))),
-                        ),
-                      ],
+                                      fontWeight: FontWeight.bold),
+                                )),
+                          ),
+                          Expanded(
+                            child: Center(
+                              child: Text(
+                                "$volume",
+                                style: context.h3(),
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              if (volume < 10) {
+                                setState(() {
+                                  volume++;
+                                });
+                              }
+                            },
+                            child: const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 12),
+                                child: Text("+",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold))),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            Text(
-              "当前语音包: product1",
-              style: context.h3(),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              "描述描述......",
-              style: context.h4(),
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              Text(
+                "当前语音包: product1",
+                style: context.h3(),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                "描述描述......",
+                style: context.h4(),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -209,9 +217,65 @@ class _AiDevicesTabPageState extends State<AiDevicesTabPage> {
               Text(
                 "我的设备",
                 style: context.h9(),
-              )
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              deviceItem()
             ]),
       ),
     ));
+  }
+
+  Widget deviceItem() {
+    return InkWell(
+      onTap: () {
+        context.push(RouterManifest.deviceInfoPage);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: CachedNetworkImage(
+                imageUrl: "https://via.placeholder.com/150",
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "设备名称",
+                    style: context.h2(),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "最近时间:2022-01-22",
+                    maxLines: 2,
+                    style: context.h4(),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
